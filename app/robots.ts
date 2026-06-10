5905af013c17ee4bb36ec1bff168896e4a37dfa0
Гафуров Самур <qsamurgnail.com@MacBook-Air-13.local>
2026-06-10 22:37:07 +0300

    kdkdkd

--- app/robots.ts
+++ app/robots.ts
@@ -1,12 +1,10 @@
-import type { MetadataRoute } from "next";
+import { MetadataRoute } from 'next'
+
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: [
-      {
-        userAgent: "*",
-        allow: "/",
-      },
-    ],
-    sitemap: "https://sdvig.app/sitemap.xml",
-  };
+    rules: {
+      userAgent: '*',
+      allow: '/',
+    },
+  }
 }
